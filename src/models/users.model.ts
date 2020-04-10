b7778176acd7c4e210192fd1dc4b9eb9cc748bae
Teresita Mora <teresita.mora@hotmail.com>
2020-04-09 23:05:00 -0500

    INIT

---
+++ src/models/users.model.ts
@@ -0,0 +1,54 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Users extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+  })
+  apellido?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  username: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'string',
+  })
+  fechaNac?: string;
+
+  @property({
+    type: 'string',
+    id: true
+  })
+  ID?: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Users>) {
+    super(data);
+  }
+}
+
+export interface UsersRelations {
+  // describe navigational properties here
+}
+
+export type UsersWithRelations = Users & UsersRelations;
